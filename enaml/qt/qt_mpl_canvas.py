721122323eb577d0b8a3f56d48f0d93c9707ba57
Thomas A Caswell <tcaswell@gmail.com>
2017-06-22 02:57:06 -0400

    FIX: import from correct mpl backend by qt version (#14)

--- enaml/qt/qt_mpl_canvas.py
+++ enaml/qt/qt_mpl_canvas.py
@@ -9,17 +9,25 @@ from atom.api import Typed
 
 from enaml.widgets.mpl_canvas import ProxyMPLCanvas
 
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
-try:
-    from matplotlib.backends.backend_qt4agg import (NavigationToolbar2QTAgg
-                                                    as NavigationToolbar2QT)
-except ImportError:
-    from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT
-
-from .QtCore import Qt
+from .QtCore import Qt, QT_VERSION
 from .QtWidgets import QFrame, QVBoxLayout
 
 from .qt_control import QtControl
+
+if QT_VERSION[0] == '4':
+    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
+    try:
+        from matplotlib.backends.backend_qt4agg import (
+            NavigationToolbar2QTAgg as NavigationToolbar2QT)
+    except ImportError:
+        from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT
+elif QT_VERSION[0] == '5':
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+    from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
+else:
+
+    raise RuntimeError("No known Matplotlib backend for qt version {} "
+                       "(as reported by qtpy.QT_VERSION)".format(QT_VERSION))
 
 
 class QtMPLCanvas(QtControl, ProxyMPLCanvas):
